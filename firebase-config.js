ce550d3f85eb3ae957e24e574e8f5803d37a3ec4
baggu4402 <tom44024402@gmail.com>
2026-06-16 18:16:26 +0900

    Add Firebase config for multiplayer testing

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "여기에 apiKey",
+  authDomain: "여기에 authDomain",
+  databaseURL: "여기에 databaseURL",
+  projectId: "여기에 projectId",
+  storageBucket: "여기에 storageBucket",
+  messagingSenderId: "여기에 messagingSenderId",
+  appId: "여기에 appId"
+};
+
+window.firebaseConfig = firebaseConfig;
