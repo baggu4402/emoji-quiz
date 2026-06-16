190241a95c1479898588170ba25c167ad075c22d
baggu4402 <tom44024402@gmail.com>
2026-06-16 18:32:01 +0900

    Add Firebase config for multiplayer testing

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "여기에 apiKey",
-  authDomain: "여기에 authDomain",
-  databaseURL: "여기에 databaseURL",
-  projectId: "여기에 projectId",
-  storageBucket: "여기에 storageBucket",
-  messagingSenderId: "여기에 messagingSenderId",
-  appId: "여기에 appId"
+  apiKey: "AIzaSyAZ6070W5ID4QSHiVbZDzo7XjvyZ7Or9Ag",
+  authDomain: "emoji-quiz-8f987.firebaseapp.com",
+  databaseURL: "https://emoji-quiz-8f987-default-rtdb.firebaseio.com/",
+  projectId: "emoji-quiz-8f987",
+  storageBucket: "emoji-quiz-8f987.firebasestorage.app",
+  messagingSenderId: "405603132790",
+  appId: "1:405603132790:web:298ad0ce5bf52c5ce8c657",
+  measurementId: "G-TERKYH15XQ"
 };
 
 window.firebaseConfig = firebaseConfig;
